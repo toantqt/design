941963eaca97365d56421304fa57063c693cdc61
toantqt <toantqt0609@gmail.com>
2021-01-28 00:28:34 +0700

    push

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import PreviewComponent from "./Components/Preview/Preview.component";
 function App() {
   return (
     <HashRouter>
-      <Route exact path="/" component={HomeComponent}></Route>
+      <Route exact path="/design24" component={HomeComponent}></Route>
       <Route
         path="/test/:link"
         render={(match) => <PreviewComponent link={match} />}
