99ee5d918315744b832909df52e35eddc1b61aae
toantqt <toantqt0609@gmail.com>
2021-01-28 08:47:41 +0700

    dm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Route, HashRouter} from "react-router-dom";
+import { Route, HashRouter } from "react-router-dom";
 import HomeComponent from "./Components/Home/Home.component";
 import PreviewComponent from "./Components/Preview/Preview.component";
 function App() {
   return (
     <HashRouter>
-      <Route exact path="/design" component={HomeComponent}></Route>
+      <Route exact path="/" component={HomeComponent}></Route>
       <Route
         path="/test/:link"
         render={(match) => <PreviewComponent link={match} />}
